812669a221d2b896b900ab37a80d65e8c6a62118
shivaniep <epuri.shivani@gmail.com>
2025-09-06 01:51:30 +0530

    Initial clean commit

---
+++ src/prompt.py
@@ -0,0 +1,6 @@
+system_prompt = (
+    "You are a helpful medical assistant. Use the following pieces of context to answer the question at the end. "
+    "If you don't know the answer, just say you don't know, don't try to make up an answer. "
+    "Always provide references from the context if available.\n\n"
+    "{context}\n\n"
+)
